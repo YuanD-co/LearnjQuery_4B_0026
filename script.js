a4188cc9232e5ae5b813ee996a7a6be9d4bbaf55
YuanD-co <yuandesfuno@gmail.com>
2026-05-18 11:30:06 +0700

    Menambahkan elemen p dan class

--- script.js
+++ script.js
@@ -3,3 +3,10 @@ $(document).ready(function() {
         let newName = $('#inputNama').val();
         let newEmail = $('#inputEmail').val();
         let newNim = $('#inputNim').val();
+
+        let pNama = $('<p></p>').text(newName).addClass('nama');
+        let pEmail = $('<p></p>').text(newEmail).addClass('email');
+        let pNim = $('<p></p>').text(newNim).addClass('nim');
+
+    });
+});
